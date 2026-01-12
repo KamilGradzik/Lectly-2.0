5cf1491a29ee94c00264c0bd5f6868f6e9d99a9f
KamilGradzik <k.gradzik97@gmail.com>
2026-01-12 15:24:41 +0100

    Intialization of backend app

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,3 @@
-using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,7 +10,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
